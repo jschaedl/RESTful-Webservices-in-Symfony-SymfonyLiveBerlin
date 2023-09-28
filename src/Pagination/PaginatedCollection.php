22c8ed143355b5356254168ca3cb278e28258c63
Jan Schädlich <schaedlich.jan@gmail.com>
2023-09-28 15:14:01 +0200

    code challenge 4 solution

--- src/Pagination/PaginatedCollection.php
+++ src/Pagination/PaginatedCollection.php
@@ -4,16 +4,28 @@ declare(strict_types=1);
 
 namespace App\Pagination;
 
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
 final class PaginatedCollection
 {
     public readonly array $items;
     public readonly int $total;
     public readonly int $count;
 
+    #[SerializedName('_links')]
+    public array $links;
+
     public function __construct(\Iterator $items, int $total)
     {
         $this->items = iterator_to_array($items);
         $this->total = $total;
         $this->count = \count($this->items);
+    }
+
+    public function addLink(string $rel, string $href): self
+    {
+        $this->links[$rel]['href'] = $href;
+
+        return $this;
     }
 }
--- src/Pagination/WorkshopCollectionFactory.php
+++ src/Pagination/WorkshopCollectionFactory.php
@@ -6,16 +6,24 @@ namespace App\Pagination;
 
 use App\Repository\WorkshopRepository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class WorkshopCollectionFactory extends PaginatedCollectionFactory
 {
     public function __construct(
-        private readonly WorkshopRepository $workshopRepository
+        private readonly WorkshopRepository $workshopRepository,
+        UrlGeneratorInterface $urlGenerator,
     ) {
+        parent::__construct($urlGenerator);
     }
 
     public function getRepository(): ServiceEntityRepositoryInterface
     {
         return $this->workshopRepository;
+    }
+
+    public function getRouteName(): string
+    {
+        return 'list_workshop';
     }
 }
